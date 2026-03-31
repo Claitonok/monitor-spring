40eef50d2f151fde619d9291fbdfeaa51b958ef3
Claitonok <claiton_santos1@hotmail.com>
2026-03-31 09:49:48 -0300

    Flash message +++

--- app/cadastro_users/page.tsx
+++ app/cadastro_users/page.tsx
@@ -8,8 +8,7 @@ import { toast } from "sonner";
 
 
 export default function Cadastro() {
-
-
+    
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
