32e35002121f06bc477349c877c51e596cfb3ec7
Claitonok <claiton_santos1@hotmail.com>
2026-04-03 15:59:48 -0300

    logout cookie

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,35 +1,26 @@
-'use client';
+// 'use client';
 
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
+// import { redirect } from "next/navigation";
+
+// export default function Logout() {
+//     document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+//     redirect('/');
+// }
+
+'use client';
 
 export const dynamic = 'force-dynamic';
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Logout() {
+  const router = useRouter();
 
-    useEffect(() => {
-        document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-        redirect('/');
-    }, []);
-    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    redirect('/');
+  useEffect(() => {
+    document.cookie = "token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    router.push("/");
+  }, [router]);
 
+  return <p>Saindo...</p>;
 }
-
-// 'use client';
-
-// export const dynamic = 'force-dynamic';
-
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-
-// export default function Logout() {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     document.cookie = "token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-//     router.push("/login");
-//   }, [router]);
-
-//   return <p>Saindo...</p>;
-// }
