c1d02df51cfc5ad3ae4427ce3fa2cf76bb237118
Claitonok <claiton_santos1@hotmail.com>
2026-04-03 15:33:47 -0300

    Deploy+++

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,25 +1,10 @@
-// 'use client';
-
-// import { redirect } from "next/navigation";
-
-// export default function Logout() {
-
-//     document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-//     redirect('/');
-
-// }
-
 'use client';
 
-export const dynamic = 'force-dynamic';
-
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Logout() {
-  useEffect(() => {
-    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    window.location.href = "/login";
-  }, []);
 
-  return <p>Saindo...</p>;
+    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    redirect('/');
+
 }
