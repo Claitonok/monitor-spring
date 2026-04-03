0ae777f99c270a864ebcb0e12f2da80c1416e271
Claitonok <claiton_santos1@hotmail.com>
2026-04-03 15:45:37 -0300

    fix logout cookie

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -14,12 +14,15 @@
 export const dynamic = 'force-dynamic';
 
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Logout() {
+  const router = useRouter();
+
   useEffect(() => {
-    document.cookie = "token=; Max-Age=0; path=/ expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    window.location.href = "/login";
-  }, []);
+    document.cookie = "token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    router.push("/login");
+  }, [router]);
 
   return <p>Saindo...</p>;
 }
