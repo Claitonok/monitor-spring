b230be39949369d54c115ed92fa4738b090f477c
Claitonok <claiton_santos1@hotmail.com>
2026-04-03 15:56:11 -0300

    logout cookie

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,28 +1,28 @@
-// 'use client';
-
-// import { redirect } from "next/navigation";
-
-// export default function Logout() {
-
-//     document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-//     redirect('/');
-
-// }
-
 'use client';
 
-export const dynamic = 'force-dynamic';
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Logout() {
-  const router = useRouter();
 
-  useEffect(() => {
-    document.cookie = "token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    router.push("/login");
-  }, [router]);
+    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    redirect('/');
 
-  return <p>Saindo...</p>;
 }
+
+// 'use client';
+
+// export const dynamic = 'force-dynamic';
+
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function Logout() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     document.cookie = "token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+//     router.push("/login");
+//   }, [router]);
+
+//   return <p>Saindo...</p>;
+// }
